709eb10a196dd8b3b9b0af7d16ac6a4297ffb660
arag0re.eth <arag0re.eth@protonmail.com>
2022-12-30 01:49:39 +0100

    added client logic

--- go.mod
+++ go.mod
@@ -3,5 +3,7 @@ module main
 go 1.15
 
 require (
+	github.com/brutella/hap v0.0.20
+	github.com/brutella/hc v1.2.5
 	howett.net/plist v1.0.0
 )
--- main/main.go
+++ main/main.go
@@ -1,139 +1,192 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
-	"math"
 	"net/http"
 
+	"github.com/brutella/hc/db"
+	"github.com/brutella/hc/hap"
+	"github.com/brutella/hc/hap/pair"
+	"github.com/brutella/hc/log"
 	"howett.net/plist"
 )
 
-type ServerInfo struct {
-	ACL            int64  `plist:"acl"`
-	DeviceID       string `plist:"deviceid"`
-	Features       int64  `plist:"features"`
-	MacAddress     string `plist:"macAddress"`
-	Model          string `plist:"model"`
-	OSBuildVersion string `plist:"osBuildVersion"`
-	Protovers      string `plist:"protovers"`
-	Srcvers        string `plist:"srcvers"`
+type PairSetupPin1Response struct {
+	Pk   []byte
+	Salt []byte
 }
 
-/**
-* Spec from:
-* https://openairplay.github.io/airplay-spec/features.html
-*
-**/
-type Features struct {
-	Video                bool //video supported
-	Photo                bool //photo supported
-	VideoFairPlay        bool //video protected with FairPlay DRM
-	VideoVolumeControl   bool //volume control supported for videos
-	VideoHTTPLiveStreams bool //http live streaming supported
-	Slideshow            bool //slideshow supported
-	//empty
-	Screen       bool //mirroring supported
-	ScreenRotate bool //screen rotation supported
-	Audio        bool //audio supported
-	//empty
-	AudioRedundant     bool //audio packet redundancy supported
-	FPSAPv2pt5_AES_GCM bool //FairPlay secure auth supported
-	PhotoCaching       bool //photo preloading supported
-	Authentication4    bool //Authentication type 4. FairPlay authentication
-	MetadataFeature1   bool //bit 1 of MetadataFeatures. Artwork.
-	MetadataFeature2   bool //bit 2 of MetadataFeatures. Progress.
-	MetadataFeature0   bool //bit 0 of MetadataFeatures. Text.
-	AudioFormat1       bool //support for audio format 1
-	AudioFormat2       bool //support for audio format 2. This bit must be set for AirPlay 2 connection to work
-	AudioFormat3       bool //support for audio format 3. This bit must be set for AirPlay 2 connection to work
-	AudioFormat4       bool //support for audio format 4
-	//empty
-	Authentication1 bool //Authentication type 1. RSA Authentication
-	//empty
-	//empty
-	HasUnifiedAdvertiserInfo bool
-	SupportsLegacyPairing    bool
-	//empty
-	//empty
-	RAOP bool //RAOP is supported on this port. With this bit set your don't need the AirTunes service
-	//empty
-	IsCarPlay                     bool //Don’t read key from pk record it is known
-	SupportsAirPlayVideoPlayQueue bool
-	SupportsAirPlayFromCloud      bool
-	//empty
-	//empty
-	//empty
-	SupportsCoreUtilsPairingAndEncryption bool //SupportsHKPairingAndAccessControl, SupportsSystemPairing and SupportsTransientPairing implies SupportsCoreUtilsPairingAndEncryption
-	//empty
-	SupportsBufferedAudio    bool //Bit needed for device to show as supporting multi-room audio
-	SupportsPTP              bool //Bit needed for device to show as supporting multi-room audio
-	SupportsScreenMultiCodec bool
-	SupportsSystemPairing    bool
-	//empty
-	//empty
-	SupportsHKPairingAndAccessControl bool
-	//empty
-	SupportsTransientPairing bool //SupportsSystemPairing implies SupportsTransientPairing
-	//empty
-	MetadataFeature4                bool //bit 4 of MetadataFeatures. binary plist.
-	SupportsUnifiedPairSetupAndMFi  bool //Authentication type 8. MFi authentication
-	SupportsSetPeersExtendedMessage bool
+func CreatePList(data map[string]string) ([]byte, error) {
+	var b bytes.Buffer
+	enc := plist.NewEncoder(&b)
+	err := enc.Encode(data)
+	if err != nil {
+		return nil, err
+	}
+	return b.Bytes(), nil
 }
 
-func getXML(url string) ([]byte, error) {
-	// Make a GET request to the specified URL
-	resp, err := http.Get(url)
+func Parse(data []byte) (map[string]interface{}, error) {
+	var plistData map[string]interface{}
+	_, err := plist.Unmarshal(data, &plistData)
 	if err != nil {
-		return nil, fmt.Errorf("Error making GET request: %v", err)
+		return nil, err
 	}
-	defer resp.Body.Close()
+	return plistData, nil
+}
 
-	// Read the response body
-	body, err := ioutil.ReadAll(resp.Body)
+func pairPinStart(b io.Reader) (io.Reader, error) {
+	return Post(b, "pair-pin-start", hap.HTTPContentTypePairingTLV8, nil)
+}
+
+func pairSetupPin1() (*PairSetupPin1Response, error) {
+	pairSetupPinRequestData, err := CreatePList(map[string]string{
+		"method": "pin",
+		"user":   "testing123",
+	})
 	if err != nil {
-		return nil, fmt.Errorf("Error reading response body: %v", err)
+		return nil, err
 	}
+	resp, err := Post(nil, "pair-setup-pin", "application/x-apple-binary-plist", pairSetupPinRequestData)
+	if err != nil {
+		return nil, err
+	}
+	data, err := ioutil.ReadAll(resp)
+	if err != nil {
+		return nil, err
+	}
+	pairSetupPin1Response, err := Parse(data)
+	if err != nil {
+		return nil, err
+	}
+	if _, okPk := pairSetupPin1Response["pk"]; okPk {
+		if _, okSalt := pairSetupPin1Response["salt"]; okSalt {
+			pkBytes := pairSetupPin1Response["pk"].([]byte)
+			saltBytes := pairSetupPin1Response["salt"].([]byte)
+			return &PairSetupPin1Response{Pk: pkBytes, Salt: saltBytes}, nil
+		}
+	}
+	return nil, fmt.Errorf("missing 'pk' and/or 'salt' keys in response")
+}
 
-	// Return the body as a byte slice
-	return body, nil
+func Post(b io.Reader, endpoint string, contentType string, data []byte) (io.Reader, error) {
+	url := fmt.Sprintf("http://192.168.1.55:7000/%s", endpoint)
+	if b != nil {
+		resp, err := http.Post(url, contentType, b)
+		fmt.Print(resp.Body, resp.Header)
+		if resp.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("Invalid status code %v", resp.StatusCode)
+		}
+		return resp.Body, err
+	} else {
+		resp, err := http.Post(url, contentType, bytes.NewReader(data))
+		fmt.Print(resp.Body, resp.Header)
+		if resp.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("Invalid status code %v", resp.StatusCode)
+		}
+		return resp.Body, err
+	}
 }
 
 func main() {
-	url := "http://192.168.1.35:7000/server-info"
-	res, err := getXML(url)
+	database, _ := db.NewDatabase("./data")
+	c, _ := hap.NewDevice("Golang Mirroring", database)
+	client := pair.NewSetupClientController("336-02-620", c, database)
+	pairPinStartRequest := client.InitialPairingRequest()
+	_, err := pairPinStart(pairPinStartRequest)
 	if err != nil {
-		fmt.Printf("Error getting XML: %v", err)
-		return
+		log.Info.Panic(err)
 	}
-	var serverInfo ServerInfo
-	format, err := plist.Unmarshal(res, &serverInfo)
+	PairSetupPin1Response, err := pairSetupPin1()
 	if err != nil {
-		panic(err)
+		log.Info.Panic(err)
 	}
-	if format == 1 {
-		//fmt.Println(format)
-		//fmt.Println(serverInfo.Features)
-		features := serverInfo.Features
-		featuresToCheck := []int64{int64(math.Pow(2, 0)), int64(math.Pow(2, 7)), int64(math.Pow(2, 9))}
-		for i, bitmask := range featuresToCheck {
-			if features&bitmask == bitmask {
-				fmt.Println("The feature set." + fmt.Sprint(i))
-			} else {
-				fmt.Println("The feature not set." + fmt.Sprint(i))
-			}
-		}
-	}
-
-	//featuresBinStr := strings.TrimLeft(strconv.FormatInt(int64(serverInfo.Features), 2), "-")
-	//featuresBits := []rune(featuresBinStr)
-	//var features Features
-	//for i := 0; i < len(featuresBits); i++ {
-	//	if featuresBits[i] == 49 && i == 0 {
-	//		features.Video = true
-	//	} else if featuresBits[i] == 49 && i == 1{
-	//		features.Photo = true
-	//	}
-	//}
-	//fmt.Println(featuresBits)
+	print(PairSetupPin1Response.Pk)
+	//startPairing()
 }
+
+//	func main() {
+//		database, _ := db.NewDatabase("./data")
+//		c, _ := hap.NewDevice("Golang Mirroring", database)
+//		client := pair.NewSetupClientController("1234", c, database)
+//		pairStartRequest := client.InitialPairingRequest()
+//		pairStartResponse, err := pairPinStart(pairStartRequest)
+//		if err != nil {
+//			log.Info.Panic(err)
+//		}
+//		var input string
+//		fmt.Print("Enter a the pin shown on your Target Device: ")
+//		fmt.Scanf("%s\n", &input)
+//		fmt.Println("You entered:", input)
+//		// 2) S -> C
+//		pairVerifyRequest, err := pair.HandleReaderForHandler(pairStartResponse, client)
+//		if err != nil {
+//			log.Info.Panic(err)
+//		}
+//
+//		// 3) C -> S
+//		pairVerifyResponse, err := pairPinStart(pairVerifyRequest)
+//		if err != nil {
+//			log.Info.Panic(err)
+//		}
+//
+//		// 4) S -> C
+//		pairKeyRequest, err := pair.HandleReaderForHandler(pairVerifyResponse, client)
+//		if err != nil {
+//			log.Info.Panic(err)
+//		}
+//
+//		// 5) C -> S
+//		pairKeyRespond, err := pairPinStart(pairKeyRequest)
+//		if err != nil {
+//			log.Info.Panic(err)
+//		}
+//
+//		// 6) S -> C
+//		request, err := pair.HandleReaderForHandler(pairKeyRespond, client)
+//		if err != nil {
+//			log.Info.Panic(err)
+//		}
+//
+//		if request != nil {
+//			log.Info.Println(request)
+//		}
+//
+//		log.Info.Println("*** Pairing done ***")
+//
+//		verify := pair.NewVerifyClientController(c, database)
+//
+//		verifyStartRequest := verify.InitialKeyVerifyRequest()
+//		// 1) C -> S
+//		verifyStartResponse, err := pairSetupPin(verifyStartRequest)
+//		if err != nil {
+//			log.Info.Panic(err)
+//		}
+//
+//		// 2) S -> C
+//		verifyFinishRequest, err := pair.HandleReaderForHandler(verifyStartResponse, verify)
+//		if err != nil {
+//			log.Info.Panic(err)
+//		}
+//
+//		// 3) C -> S
+//		verifyFinishResponse, err := pairSetupPin(verifyFinishRequest)
+//		if err != nil {
+//			log.Info.Panic(err)
+//		}
+//
+//		// 4) S -> C
+//		last_request, err := pair.HandleReaderForHandler(verifyFinishResponse, verify)
+//		if err != nil {
+//			log.Info.Panic(err)
+//		}
+//
+//		if last_request != nil {
+//			log.Info.Println(last_request)
+//		}
+//
+//		log.Info.Println("*** Key Verification done ***")
+//	}
