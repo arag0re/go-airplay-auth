942555eda9d17cd858d55952114202ac4f929a18
arag0re.eth <arag0re.eth@protonmail.com>
2023-01-02 21:24:33 +0100

    cleaned up from bs

--- go.mod
+++ go.mod
@@ -3,10 +3,6 @@ module main
 go 1.15
 
 require (
-	github.com/brutella/hap v0.0.20
 	github.com/brutella/hc v1.2.5
-	github.com/opencoff/go-srp v0.6.0
-	golang.org/x/crypto v0.0.0-20220131195533-30dcbda58838 // indirect
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	howett.net/plist v1.0.0
 )
--- main/main.go
+++ main/main.go
@@ -4,60 +4,20 @@ import (
 	"bufio"
 	"bytes"
 	"crypto/ecdsa"
-	_ "crypto/ecdsa"
-	_ "crypto/ed25519"
 	"crypto/elliptic"
-	_ "crypto/elliptic"
 	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	_ "encoding/binary"
-	"encoding/hex"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"math"
-	"math/big"
 	"net"
 	"net/http"
 	"strconv"
 	"strings"
 
 	"github.com/brutella/hc/log"
-	_ "github.com/opencoff/go-srp"
 	"howett.net/plist"
 )
-
-var ClientID = ""
-
-const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-
-type NumberStore struct {
-	pk   *big.Int
-	salt *big.Int
-}
-type PairSetupPin1Response struct {
-	Pk   []byte
-	Salt []byte
-}
-
-type PairSetupPin2Response struct {
-	Pk    []byte
-	Proof []byte
-}
-
-func randomString(length int) string {
-	b := make([]byte, length)
-	_, err := rand.Read(b)
-	if err != nil {
-		panic(err)
-	}
-	s := make([]byte, length)
-	for i, v := range b {
-		s[i] = charset[v%byte(len(charset))]
-	}
-	return string(s)
-}
 
 func post(socket net.Conn, path string, contentType string, CSeq string, data []byte) ([]byte, error) {
 	postErr := postData(socket, path, contentType, CSeq, data)
@@ -159,26 +119,6 @@ func readResponse(conn net.Conn) (*http.Response, error) {
 	return resp, nil
 }
 
-func createPListStep1(data map[string]string) ([]byte, error) {
-	var b bytes.Buffer
-	enc := plist.NewEncoder(&b)
-	err := enc.Encode(data)
-	if err != nil {
-		return nil, err
-	}
-	return b.Bytes(), nil
-}
-
-func createPListStep2(data map[string][]byte) ([]byte, error) {
-	var b bytes.Buffer
-	enc := plist.NewEncoder(&b)
-	err := enc.Encode(data)
-	if err != nil {
-		return nil, err
-	}
-	return b.Bytes(), nil
-}
-
 func Parse(data []byte) (map[string]interface{}, error) {
 	var plistData map[string]interface{}
 	_, err := plist.Unmarshal(data, &plistData)
@@ -186,34 +126,6 @@ func Parse(data []byte) (map[string]interface{}, error) {
 		return nil, err
 	}
 	return plistData, nil
-}
-
-func generateNewAuthToken() string {
-	clientID := randomString(16)
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		panic(err)
-	}
-	privateKeyDER := x509.MarshalPKCS1PrivateKey(privateKey)
-	privateKeyHex := hex.EncodeToString(privateKeyDER)
-	return clientID + "@" + privateKeyHex
-}
-
-func GenerateKey() (*ecdsa.PrivateKey, error) {
-	return ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-}
-
-func ECDHSharedKey(privKey *ecdsa.PrivateKey, pubKey *ecdsa.PublicKey) ([]byte, error) {
-	x, y := pubKey.Curve.ScalarMult(pubKey.X, pubKey.Y, privKey.D.Bytes())
-	if x == nil {
-		return nil, fmt.Errorf("invalid public key")
-	}
-	xBytes := x.Bytes()
-	yBytes := y.Bytes()
-	sharedKey := make([]byte, len(xBytes)+len(yBytes))
-	copy(sharedKey[:len(xBytes)], xBytes)
-	copy(sharedKey[len(xBytes):], yBytes)
-	return sharedKey, nil
 }
 
 func pairPinStart(addr string) {
@@ -265,107 +177,6 @@ func doPairSetup(socket net.Conn) ([]byte, error) {
 		fmt.Println("not all neccessary features are enabled on AirServer")
 		return nil, nil
 	}
-}
-
-func doPairSetupPin1(socket net.Conn) (*PairSetupPin1Response, error) {
-	pairSetupPinRequestData, err := createPListStep1(map[string]string{
-		"method": "pin",
-		"user":   ClientID,
-	})
-	if err != nil {
-		return nil, err
-	}
-	postErr := postData(socket, "/pair-setup-pin", "application/x-apple-binary-plist", "0", pairSetupPinRequestData)
-	if postErr != nil {
-		return nil, postErr
-	}
-	resp, err := readResponse(socket)
-	if err != nil {
-		log.Info.Panic(err)
-		return &PairSetupPin1Response{}, err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Info.Panic(err)
-		return &PairSetupPin1Response{}, err
-	}
-	pairSetupPin1Response, err := Parse(body)
-	if err != nil {
-		return nil, err
-	}
-	if _, okPk := pairSetupPin1Response["pk"]; okPk {
-		if _, okSalt := pairSetupPin1Response["salt"]; okSalt {
-			pkBytes := pairSetupPin1Response["pk"].([]byte)
-			saltBytes := pairSetupPin1Response["salt"].([]byte)
-			return &PairSetupPin1Response{Pk: pkBytes, Salt: saltBytes}, nil
-		}
-	}
-	return nil, fmt.Errorf("missing 'pk' and/or 'salt' keys in response")
-}
-
-//
-//func doPairSetupPin2(socket net.Conn, publicClientValueA []byte, clientEvidenceMessageM1 []byte) (PairSetupPin2Response, error) {
-//	pairSetupPinRequestData, err := createPListStep2(map[string][]byte{
-//		"pk":    publicClientValueA,
-//		"proof": clientEvidenceMessageM1,
-//	})
-//	if err != nil {
-//		return PairSetupPin2Response{}, err
-//	}
-//	pairSetupPin2ResponseBytes, err := Post(nil, "/pair-setup-pin", "application/x-apple-binary-plist", pairSetupPinRequestData)
-//	if err != nil {
-//		return PairSetupPin2Response{}, err
-//	}
-//	data, err := ioutil.ReadAll(pairSetupPin2ResponseBytes)
-//	if err != nil {
-//		return PairSetupPin2Response{}, err
-//	}
-//	pairSetupPin2Response, err := Parse(data)
-//	if err != nil {
-//		return PairSetupPin2Response{}, err
-//	}
-//
-//	if _, ok := pairSetupPin2Response["proof"]; !ok {
-//		return PairSetupPin2Response{}, fmt.Errorf("missing proof key in response")
-//	}
-//
-//	proof, ok := pairSetupPin2Response["proof"].([]byte)
-//	if !ok {
-//		return PairSetupPin2Response{}, fmt.Errorf("proof key in response is not of type []byte")
-//	}
-//
-//	return PairSetupPin2Response{Proof: proof}, nil
-//}
-
-func doPairing() {
-	socket, err := openTCPConnection("192.168.1.35:7000")
-	if err != nil {
-		log.Info.Panic(err)
-	}
-	//pairPinStartAlt("192.168.1.35:7000")
-	_ = generateNewAuthToken()
-	PairSetupPin1Response, err := doPairSetupPin1(socket)
-	if err != nil {
-		log.Info.Panic(err)
-	}
-	//PairSetupPin2Response, err := doPairSetupPin2(socket, nil, nil)
-	//if err != nil {
-	//	log.Info.Panic(err)
-	//}
-	//fmt.Println(PairSetupPin2Response.Proof)
-	pk := new(big.Int)
-	salt := new(big.Int)
-	pk.SetBytes(PairSetupPin1Response.Pk)
-	salt.SetBytes(PairSetupPin1Response.Salt)
-	var ledger NumberStore
-	ledger.pk = pk
-	ledger.salt = salt
-	fmt.Println("pk: ", ledger.pk)
-	fmt.Println("salt: ", ledger.salt)
-	//fmt.Println("Salt: " + hex.EncodeToString(PairSetupPin1Response.Salt))
-	//fmt.Println("PK lengt: " + fmt.Sprintf("%d", len(PairSetupPin1Response.Pk)))
-	//fmt.Println("Salt length: " + fmt.Sprintf("%d", len(PairSetupPin1Response.Salt)))
 }
 
 func checkForFeatures(socket net.Conn, features []int64) bool {
